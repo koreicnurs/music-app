01c3e15ced81f59616625e504f2e75a32b705296
koreicnurs <koreicnurs@gmail.com>
2022-09-11 14:44:51 +0600

    created connect to mongodb

---
+++ api/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const artist = require('./app/artist');
+const config = require('./config')
+
+const app = express();
+const port = 8000;
+
+app.use(express.static('public'));
+app.use(express.json());
+app.use(cors());
+
+app.use('/artist', artist);
+
+const run = async () => {
+    await mongoose.connect(config.mongo.db, config.mongo.options) ;
+
+    app.listen(port, () => {
+        console.log(`Server started on ${port} port!`);
+    });
+
+    process.on('exit', () => {
+        mongoose.disconnect();
+        console.log('MongoDb disconnected');
+    });
+};
+
+run().catch(e => console.log(e));
+
